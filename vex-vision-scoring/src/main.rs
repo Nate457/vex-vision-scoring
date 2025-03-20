0d647aaa7f1c9e5ae2e7f29fb2dbd39364bc5932
Nate457 <82827807+Nate457@users.noreply.github.com>
2025-03-20 15:24:06 -0400

    Set up local host template code

--- vex-vision-scoring/src/main.rs
+++ vex-vision-scoring/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+// import Rocket
+#[macro_use] extern crate rocket;
+
+// this is our get route which will be requested at the "/" location wherever it is mounted
+#[get("/")]
+fn say_hello() -> &'static str {
+  "Hello, welcome to the api!"
+}
+
+// start the web server and mount our get route at "/api". Can replace /api with anything
+// or just leave it as "/" as the default location
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/api", routes![say_hello])
+        .configure(rocket::Config {
+            address: "0.0.0.0".parse().unwrap(),  // Bind to all interfaces
+            port: 8000,  // Set the port to 8000
+            ..rocket::Config::default()  // Use default settings for other configurations
+        })
 }
